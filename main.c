a07fc107ad7f2a4ed726faf098380ad7789772e8
Jaewon Chung <jaewonchung7@gmail.com>
2025-02-13 00:14:05 +0900

    Changed main.c for testing mpu
    Signed-off-by: Jaewon Chung <jaewonchung7@gmail.com>

--- main.c
+++ main.c
@@ -9,8 +9,23 @@ int main() {
 
 	init_i2c(PICO_DEFAULT_I2C, PICO_DEFAULT_I2C_SDA_PIN,PICO_DEFAULT_I2C_SCL_PIN);
 
+	initMPU(PICO_DEFAULT_I2C,ACCEL_16G,GYRO_2000_DPS,DLPF_none);
 
 	printf("Hello, world!\n");
+
+	float accel[3]={0};
+	float gyro[3]={0};
+	float temp={0};
+
+	while(1){
+		readMPU(PICO_DEFAULT_I2C,accel,gyro,&temp, ACCEL_16G,GYRO_2000_DPS);
+
+		printf("ax:%.2f ay:%.2f az:%.2f gx:%.2f gy:%.2f gz:%.2f t:%.1f\n", 
+				accel[0], accel[1], accel[2], 
+				gyro[0], gyro[1], gyro[2], 
+				temp);
+		sleep_ms(100);  // Add small delay to not flood the console
+	}
 
 	return 0;
 }
