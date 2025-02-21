e02dad44d817e5bb89ec4e24200313c6680e8666
2024-13753 <hde831@snu.ac.kr>
2025-02-21 16:27:52 +0900

    ㄴㄹㄴㅇㄴㅁㄴ

--- main.c
+++ main.c
@@ -1,26 +1,24 @@
 #include <stdio.h>
 #include "pico/stdlib.h"
-#include "pico/time.h"
-#include "motor/motor.h"
+// #include "pico/time.h"
+// #include "motor/motor.h"
 
 int main() {
 	stdio_init_all();
+
 	uint8_t pins[4]= {10, 11, 12, 13};
 	initMotors(pins);
     printf("Hello, world!\n");
-	int speeds[4]={10, 11,0,0};
-	
+	uint16_t speeds[4]={0, 0,0,0};
+	uint16_t speed=0;
 	while(1){
-		*speeds=0;
-		*(speeds+1)=0;
+		scanf("%hu",&speed);
+		speeds[0]=speed;
+		speeds[1]=speed;
+	
 		setSpeeds(speeds);
-		printf("Testing1");
-		sleep_ms	(1000);
-		*speeds=100;
-		*(speeds+1)=100;
-		setSpeeds(speeds);
-		printf("Testing2");
-		sleep_ms	(1000);
+		printf("Testing1\n");
+		sleep_ms(1000);
 	}
 	return 0;
 }
